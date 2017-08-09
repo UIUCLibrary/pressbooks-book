c1f3ca2cfc184f25056146c53c60bd3c91465e2f
raa2 <raa2@illinois.edu>
2017-08-09 16:52:02 -0500

    header style 1.4

--- header.php
+++ header.php
@@ -17,10 +17,10 @@
 
 	<?php wp_head(); ?>
 </head>
-<header style="background-color:navy;">
+<header style="background-color:navy;border:0px;">
 <h1><a href="https://pb-sandbox.library.illinois.edu">
 	<img align="left" src="<?php echo get_template_directory_uri(); ?>/assets/images/iopn_color.png" alt="IOPN Network"></img>
-	<img align="left" src="<?php echo get_template_directory_uri(); ?>/assets/images/ui_library.png" alt="university branding"></img></a></h1>
+	<img align="bottom left;" src="<?php echo get_template_directory_uri(); ?>/assets/images/ui_library.png" alt="university branding"></img></a></h1>
 </header>
 <?php
 if ( is_front_page() ) {
