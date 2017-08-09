3b1c4e5d2983eb2d7ca09133a94214ee75545d33
raa2 <raa2@illinois.edu>
2017-08-09 17:21:16 -0500

    header style 1.9

--- header.php
+++ header.php
@@ -39,10 +39,10 @@ if ( $paged >= 2 || $page >= 2 ) {
 <?php wp_head(); ?>
 
 </head>
-<header>
-<h1><a href="https://pb-sandbox.library.illinois.edu">
+<header style="background-color:#24478f">
+<a href="https://pb-sandbox.library.illinois.edu">
 	<img align="left" src="<?php echo get_template_directory_uri(); ?>/assets/images/iopn_navy.png" alt="IOPN Network" width="186" height="100" ></img>
-	<img align="text-bottom" src="<?php echo get_template_directory_uri(); ?>/assets/images/ui_library.png" alt="university branding" width="286" height="200" ></img></a></h1>
+	<img align="text-bottom" src="<?php echo get_template_directory_uri(); ?>/assets/images/ui_library.png" alt="university branding" width="286" height="200" ></img></a>
 </header>
 <?php if ( is_front_page() ) {
 	$schema = 'itemscope itemtype="http://schema.org/Book" itemref="about alternativeHeadline author copyrightHolder copyrightYear datePublished description editor image inLanguage keywords publisher" ';
