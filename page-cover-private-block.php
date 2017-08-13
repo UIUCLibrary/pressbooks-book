efad46e1dbe18a0fa65283dc8d03838aec4a4c5b
raa2 <raa2@illinois.edu>
2017-08-13 12:39:22 -0500

    fix private block

--- page-cover-private-block.php
+++ page-cover-private-block.php
@@ -1,10 +1,8 @@
 <<<<<<< HEAD
 <div class="right-block">
 		<a href="http://pressbooks.com" class="pressbooks-brand"><img src="<?php bloginfo( 'template_url' ); ?>/images/pressbooks-branding-2x.png" alt="pressbooks-branding" width="186" height="123" /> <span><?php _e( 'Make your own books on Pressbooks', 'pressbooks-book' ); ?></span></a>
-=======
 	<div class="right-block">
 		<a href="http://pressbooks.com" class="pressbooks-brand"><img src="<?php bloginfo( 'template_url' ); ?>/dist/images/pressbooks-branding-2x.png" alt="pressbooks-branding" width="186" height="123" /> <span><?php _e( 'Make your own books on Pressbooks', 'pressbooks-book' ); ?></span></a>
->>>>>>> 0030af3082d23900fb37cf244422c4ccc7622708
 	</div>	
 	
 <section class="private-block">
