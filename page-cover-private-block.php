b1f34fdac27b5cc9bb689cf5338ab8a914805ce0
raa2 <raa2@illinois.edu>
2017-08-13 12:45:08 -0500

    header style 3.0

--- page-cover-private-block.php
+++ page-cover-private-block.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <div class="right-block">
 		<a href="http://pressbooks.com" class="pressbooks-brand"><img src="<?php bloginfo( 'template_url' ); ?>/images/pressbooks-branding-2x.png" alt="pressbooks-branding" width="186" height="123" /> <span><?php _e( 'Make your own books on Pressbooks', 'pressbooks-book' ); ?></span></a>
 	<div class="right-block">
