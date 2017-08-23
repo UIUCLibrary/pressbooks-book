fc5a059014b02f7469be0ff42d5f24533111a61e
raa2 <raa2@illinois.edu>
2017-08-22 20:55:29 -0500

    header 1.8

--- header.php
+++ header.php
@@ -21,7 +21,7 @@
 <header class="iopn-header">
 <a href="http://library.illinois.edu"><img id="uiuc-brand" src="<?php echo get_template_directory_uri(); ?>/assets/images/ui_library.png" alt="university branding"></img></a>
 <a href="https://iopn.library.illinois.edu/"><img id="iopn-color" src="<?php echo get_template_directory_uri(); ?>/assets/images/iopn_color.png" alt="IOPN Network"></img></a>
-Illinois Open Publishing Network
+<p id="iopn-text">Illinois Open Publishing Network</p>
 
 <div class="log-wrap">	<!-- Login/Logout -->
 		<?php if ( ! is_single() ) : ?>
