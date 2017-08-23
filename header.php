ebfe508c7c0d42bba74474f6ec5e4f6f129654f0
raa2 <raa2@illinois.edu>
2017-08-22 19:37:09 -0500

    Header 1.1

--- header.php
+++ header.php
@@ -39,9 +39,10 @@ if ( $paged >= 2 || $page >= 2 ) {
 <?php wp_head(); ?>
 
 </head>
-	<header class="iopn-header">
+	
+<header class="iopn-header">
 <a href="http://library.illinois.edu"><img id="uiuc-brand" src="<?php echo get_template_directory_uri(); ?>/assets/images/ui_library.png" alt="university branding"></img></a>
-<a href="https://iopn.library.illinois.edu/"><img id="iopn-color" src="<?php echo get_template_directory_uri(); ?>/assets/images/iopn_color.png" alt="IOPN Network"></img></a> <?php echo "Illinois Open Publishing Network"?>
+<a href="https://iopn.library.illinois.edu/"><img id="iopn-color" src="<?php echo get_template_directory_uri(); ?>/assets/images/iopn_color.png" alt="IOPN Network"></img><?php echo "Illinois Open Publishing Network"?></a> 
 
 <div class="log-wrap">	<!-- Login/Logout -->
 		<?php if ( ! is_single() ) : ?>
