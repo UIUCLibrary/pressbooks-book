c15c939e5338ebd74c3ba657e4261f0687470b21
Alex Dryden <adryden3@illinois.edu>
2025-04-25 16:08:46 -0400

    manage the addition of the custom copyright from doLicense

--- inc/helpers/namespace.php
+++ inc/helpers/namespace.php
@@ -698,9 +698,7 @@ function copyright_license( $show_custom_copyright = true ) {
 	if ( ! $has_custom_copyright || ( $has_custom_copyright && ! $all_rights_reserved ) || ! $show_custom_copyright ) {
 		$html .= \PressbooksBook\Helpers\do_license( $metadata, $show_custom_copyright );
 	}
-	if ( $has_custom_copyright && $show_custom_copyright ) {
-		$html .= '<div class="license-attribution">' . $metadata['pb_custom_copyright'] . '</div>';
-	}
+
 
 	return $html;
 }
